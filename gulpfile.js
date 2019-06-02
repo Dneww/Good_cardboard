a4089b5a14a53b73ff8d8fea7f59e65cb8dfb542
Dneww <47459252+Dneww@users.noreply.github.com>
2019-06-02 17:56:51 +0500

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var bs = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', gulp.parallel ('sass'), function () {
+
+	bs.init({
+		server: "./src"
+	});
+
+	gulp.watch("src/sass/*.sass", gulp.parallel ('sass'));
+	gulp.watch("src/*.html").on('change', bs.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function () {
+	return gulp.src("src/sass/*.sass")
+		.pipe(sass())
+		.pipe(gulp.dest("src/css"))
+		.pipe(bs.stream());
+});
+
+gulp.task('default', gulp.parallel ('serve', 'sass', 'bs'));
---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+   $('.header_contacts_button').on("click", function() {
+      $('.overlay').show()
+   });
+
+    $('.popup-close').on("click", function() {
+       $('.overlay').hide();
+    });
+});
+$(document).ready(function () {
+   $('.products-button_').on("click", function () {
+      $('.overlay').show()
+   });
+
+   $('.popup-close').on("click", function () {
+      $('.overlay').hide();
+   });
+});
